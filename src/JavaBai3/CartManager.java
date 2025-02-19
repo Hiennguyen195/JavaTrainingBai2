5f75a89d33225ea1afbc2c04c9db1228943cec57
Hiennguyen195 <nguyenhien19052000@gmail.com>
2025-02-19 15:54:34 +0700

    JavaTraining

--- src/JavaBai3/CartManager.java
+++ src/JavaBai3/CartManager.java
@@ -6,7 +6,19 @@ public class CartManager {
     LinkedList<Product> productsInCart = new LinkedList<Product>();
 
     //Thêm sản phẩm vào giỏ hàng
-    public void addProduct(Product p) {
+    public void addProductToCart(Product p) {
         productsInCart.add(p);
     }
+    //Xóa sản phẩm ra khỏi giỏ hàng
+    public void removeProductFromCart(Product p) {
+        productsInCart.remove(p);
+    }
+    //Hiển thị thông tin giỏ hàng
+    public void showCartInfo() {
+        for (Product p : productsInCart) {
+            System.out.println(p);
+        }
+    }
+
+
 }
