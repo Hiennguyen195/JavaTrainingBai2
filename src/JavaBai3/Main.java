97aa124dab66e914259cb00dbae7f06e0715b6f5
Hiennguyen195 <nguyenhien19052000@gmail.com>
2025-02-19 14:27:07 +0700

    JavaTraining

--- src/JavaBai3/Main.java
+++ src/JavaBai3/Main.java
@@ -3,6 +3,8 @@ package JavaBai3;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("========================================");//Ngăn cách dòng
+
         Product p1 = new Product("Product1",12000, 500, "Category1");
         Product p2 = new Product("Product2",13000, 600, "Category1");
         Product p3 = new Product("Product3",14000, 300, "Category2");
@@ -23,9 +25,19 @@ public class Main {
 
         System.out.println("========================================");//Ngăn cách dòng
 
-        //Xóa một sản phẩm ra khỏi productsList
-        pm.removeProduct(p1);
-        //In ra danh sách sau khi xóa
-        pm.showProducts();
+//        //Xóa một sản phẩm ra khỏi productsList
+//        pm.removeProduct(p1);
+//        //In ra danh sách sau khi xóa
+//        pm.showProducts();
+//
+//        System.out.println("========================================");//Ngăn cách dòng
+//
+//        //In ra sản phẩm có giá cao nhất trong productsList
+//        pm.highestPriceProduct();
+//
+//        System.out.println("========================================");//Ngăn cách dòng
+
+        //Lọc sản phẩm theo Category
+        pm.filterByCategory("Category1");
     }
 }
--- src/JavaBai3/ProductManager.java
+++ src/JavaBai3/ProductManager.java
@@ -1,5 +1,6 @@
 package JavaBai3;
 
+import java.util.Collections;
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
@@ -20,5 +21,32 @@ public class ProductManager {
     //Phương thức xóa sản phẩm ra khỏi productsList
     public void removeProduct(Product product) {
         productsList.remove(product);
+    }
+    //Phương thức trả về sản phẩm có giá cao nhất
+    public void highestPriceProduct() {
+        double highestPrice = 0;
+        int x = 0;
+
+        for (int i = 0; i<productsList.size(); i++) {
+            Product prd = productsList.get(i);
+            if (prd.getProductPrice() > highestPrice) {
+                highestPrice = prd.getProductPrice();
+                x = i;
+            }
+        }
+        System.out.println("Sản phẩm có giá cao nhất trong danh sách là: " +"\n"+ productsList.get(x).toString());
+    }
+    //Phương thức lọc product theo một Category cụ thể
+    public void filterByCategory(String category) {
+        List<Product> filteredProducts = new ArrayList<>();
+        for (Product product : productsList) {
+            if (product.getProductCategory().equals(category)) {
+                filteredProducts.add(product);
+            }
+        }
+        System.out.format("Sản phẩm thuộc %s là: \n", category);
+        for (Product product : filteredProducts) {
+            System.out.println(product.toString());
+        }
     }
 }
