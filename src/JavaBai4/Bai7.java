cbb118f7a0b29fc55137c3345adef7df6e997307
Hiennguyen195 <nguyenhien19052000@gmail.com>
2025-02-26 15:41:07 +0700

    JavaTraining

--- src/JavaBai4/Bai7.java
+++ src/JavaBai4/Bai7.java
@@ -1,10 +1,27 @@
 package JavaBai4;
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Bai7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        List<Person> peopleList = Arrays.asList(
+                new Person("Hien", 18),
+                new Person("Anh", 25),
+                new Person("Hung", 30),
+                new Person("Hien", 25)
+        );
 
+        System.out.println("List of people: " + peopleList);
+
+        Map<String, Person> personMap = peopleList.stream()
+                .collect(Collectors.toMap(Person::getName,
+                        person -> person,
+                        (existing, replacement) -> existing  // Giữ giá trị cũ nếu trùng key
+                ));
+
+        personMap.forEach((name, person) -> System.out.println(name + "=> " + person));
     }
 }
